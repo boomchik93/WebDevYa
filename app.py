8e5f44913e191d295dfdb164a78774b78d3719a8
boomchik93 <fedyazhukov06@gmail.com>
2025-04-25 15:12:29 +0500

    Двойная защита

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/list_prof/<param>')
+def list_prof(param):
+    professions = [
+        "инженер-исследователь",
+        "пилот",
+        "строитель",
+        "экзобиолог",
+        "врач",
+        "инженер по терраформированию",
+        "климатолог",
+        "специалист по радиационной защите",
+        "астротеолог",
+        "гляциолог",
+        "инженер жизнеобеспечения",
+        "метеоролог",
+        "оператор марсохода",
+        "киберинженер",
+        "штурман",
+        "пилот дронов"
+    ]
+    app.logger.info(f"Получен запрос на /list_prof/{param}")
+    if param == 'ol':
+        return render_template('listl.html', typel='ol', profes=professions)
+    elif param == 'ul':
+        return render_template('listl.html', typel='ul', profes=professions)
+    else:
+        return "Ошибка: используйте /list_prof/ol или /list_prof/ul", 404
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8000, host='127.0.0.1', debug=False)
