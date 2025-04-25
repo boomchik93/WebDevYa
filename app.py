a8164d7c2b44c60fb1a858ddee9fe1d68fce178f
boomchik93 <fedyazhukov06@gmail.com>
2025-04-25 15:22:50 +0500

    По каютам!

--- app.py
+++ app.py
@@ -37,5 +37,11 @@ def login():
     return render_template('login.html')
 
 
+@app.route('/distribution')
+def distribution():
+    astronauts = ['Иванов', 'Петров', 'Сидоров']
+    return render_template('distribution.html', astronauts=astronauts)
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1', debug=False)
